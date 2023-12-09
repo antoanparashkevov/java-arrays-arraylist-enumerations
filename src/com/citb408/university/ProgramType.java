15ed7b66a091a3976fb90b54d2a8d097f39e2172
“antoanparashkevov” <tonkata1505@gmail.com>
2023-12-09 17:46:34 +0200

    Extend the enumerable type of data and add additional properties for each constant

--- src/com/citb408/university/ProgramType.java
+++ src/com/citb408/university/ProgramType.java
@@ -1,5 +1,14 @@
 package com.citb408.university;
 
 public enum ProgramType {
-    MAJOR, MINOR
+    MAJOR(240), MINOR(120);//additional properties attached to the constants
+    private int minimalAmountOfCredits;
+
+    ProgramType(int minimalAmountOfCredits) {
+        this.minimalAmountOfCredits = minimalAmountOfCredits;
+    }
+
+    public int getMinimalAmountOfCredits() {
+        return minimalAmountOfCredits;
+    }
 }
